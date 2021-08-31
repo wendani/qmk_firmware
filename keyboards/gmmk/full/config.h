61de118f810d1670918d2c937539be99f13327ba
Glory <gloryhzw@gmail.com>
2021-08-31 23:09:42 +0800

    [Glorious GMMK Full (2021)] Enable OpenRGB with some hacks
    1. GMMK 2021 FULL with two SNLED2735 RGB chips by SPI
    2. openrgb support with glitches (slow performance / display error)
    
    Tuning
    1. Tightly reduce stack size
    USE_EXCEPTIONS_STACKSIZE = 0xd0
    USE_PROCESS_STACKSIZE = 0x200
    2. Add const to g_led_config[]
    3. -Os compile option and LTO_ENABLE=yes
    
    4. Self-defined rand() for animations using rand()
    5. Reduce idle thread stack size and disable registry in chconf.h
    
    Commit amended to split out OpenRGB functionality from core additions by Adam Honse <calcprogrammer1@gmail.com

--- keyboards/gmmk/full/config.h
+++ keyboards/gmmk/full/config.h
@@ -53,3 +53,9 @@
 #include "config_led.h"
 
 #define RGB_DISABLE_WHEN_USB_SUSPENDED true
+
+/* change the g_led_config global to const for RAM saving */
+#define RGB_MATRIX_CONST_LED_CONFIG
+
+/* use unbuffered direct mode in OpenRGB protocol */
+#define OPENRGB_DIRECT_MODE_UNBUFFERED
