02635ceb8771f19c31b6b9392a9c25a01222f5b4
Wenda Ni <wonda.ni@gmail.com>
2024-12-28 10:27:05 +0000

    fn
    Signed-off-by: Wenda Ni <wonda.ni@gmail.com>

--- keyboards/anavi/macropad8/keymaps/default/keymap.c
+++ keyboards/anavi/macropad8/keymaps/default/keymap.c
@@ -12,8 +12,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_FN] = LAYOUT_ortho_2x4(
-     UG_TOGG, UG_NEXT, RGB_M_R, RGB_M_SN,
-     BL_TOGG, BL_STEP, BL_BRTG, _______
+     UG_TOGG, UG_NEXT, RGB_M_TW, RGB_M_X,
+     G(KC_X), BL_STEP, BL_BRTG, _______
   )
 };
 
