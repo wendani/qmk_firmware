d83d7ad67b717e3287be7eeaeb9fece7bd946685
Wenda Ni <wonda.ni@gmail.com>
2024-12-28 11:49:52 +0000

    Fix compile error
    Signed-off-by: Wenda Ni <wonda.ni@gmail.com>

--- keyboards/anavi/macropad8/keymaps/default/keymap.c
+++ keyboards/anavi/macropad8/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FN] = LAYOUT_ortho_2x4(
      UG_TOGG, UG_NEXT, BL_STEP, BL_BRTG,
-     LG(KC_Z), UG_PREV, LSG(KC_Z), _______
+     G(KC_Z), UG_PREV, LSG(KC_Z), _______
   )
 };
 
